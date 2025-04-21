219bec178b1876410c9ee83d1eaf9a23813ea213
Manivannan Subramaniyan <msubramaniyan@ds.bhsai.net>
2025-04-21 10:00:34 -0400

    added new utility

--- bin_and_avg_data_by_time.m
+++ bin_and_avg_data_by_time.m
@@ -1,17 +1,17 @@
-function bin_avg = bin_and_avg_data_by_time(x,x_time,time_bin_edges,varargin)
+function bin_avg = bin_and_avg_data_by_time(X,x_time,time_bin_edges,varargin)
 % bin_and_avg_data_by_time(x,x_time,time_bin_edges) - average time series
 % data points falling within each bin of time bin edges.
 %
-% Mani Subramaniyan 2023-02-17
+% Mani Subramaniyan 2023-02-17/2024-12-16
 args.omitnan = true;
 args = parse_var_args(args,varargin{:});
 time_bin_ind = discretize(x_time,time_bin_edges);
 nBins = length(time_bin_edges)-1;
-bin_avg = nan(nBins,1);
+bin_avg = nan(size(X,1),nBins);
 for iBin = 1:nBins
     if args.omitnan
-        bin_avg(iBin,1) = mean(x(time_bin_ind==iBin),'omitnan');
+        bin_avg(:,iBin) = mean(X(:,time_bin_ind==iBin),2,'omitnan');
     else
-        bin_avg(iBin,1) = mean(x(time_bin_ind==iBin),'includenan');
+        bin_avg(:,iBin) = mean(X(:,time_bin_ind==iBin),2,'includenan');
     end
 end
